4ece5737ea873b361eda143cd59779578562d43b
Shawn Gluchacki <Shawn@MacBook-Pro-2.local>
2020-05-08 16:13:32 -0600

    add and format dictionary, some foundational work to main.js

--- js/main.js
+++ js/main.js
@@ -3,41 +3,41 @@
 // NO PLAYER TRACKER --- this is going to be boggle solitaire, no win logic necessary either
 
 // state variables
-// board
-// score
-// foundWords --- to avoid using the same word for more than one play
-// usedLetters --- changes after every click
-// timer 
-// wordInProgress --- word that is being built by clicks
+let board;
+let score;
+let foundWords;  //to avoid using the same word for more than one play
+let usedLetters; // changes after every click during a turn
+let timer; 
+let wordInProgress; // word that is being built by clicks
 
 
 // cached element references
-// boardEls --- each div on the board
-// msgEl --- for messages like 'That's not a word, silly!' and 'Time's up.'
-// submit word --- button for checking word
-// replay --- button that pops up after game ends
-// score --- to display score
-// foundWords --- maybe. just displays list of already found words
-// timer --- its a timer
+// $boardEls; // each div on the board
+// $msgEl; // for messages like 'That's not a word, silly!' and 'Time's up.'
+// $submitButton; // button for checking word
+// $replayButton; // button that pops up after game ends
+// $core; // to display score
+// $foundWords; // maybe. just displays list of already found words
+// $timer; // its a timer
 
 
 // event listeners
-// board click --- hope you have fast fingers, this is going to be the only input method
-// replay click --- calls init
+// boardClick; // hope you have fast fingers, this is going to be the only input method
+// replayClick; // calls init
 
 
 // functions
-// init --- makes 4x4 board with random letters using Math.random and the index of an alphabet array
-// render --- displays letters on board, updates score, displays 'words in progress' displays appropriate messages, displays found words
-// wordCheck --- checks word against dictionary for validity and against foundWords - called when submit word button clicked
-// clickBoard --- establishes what clicks are valid, and logs usedLetters and words in progress
-// addScore --- when words are found and valid, adds to score based on word length. Establish score based on length of word in the dictionary to account for the Qu tile
-// timer --- counts down from set time limit
-// gameOver --- upon timer end ends board click event, reveals final score, and displays replay button
+function init() {}// makes 4x4 board with random letters using Math.random and the index of an alphabet array
+function render() {} // displays letters on board, updates score, displays 'words in progress' displays appropriate messages, displays found words
+function wordCheck() {} // checks word against dictionary for validity and against foundWords - called when submit word button clicked
+function clickBoard() {} // establishes what clicks are valid, and logs usedLetters and words in progress
+function addScore() {} // when words are found and valid, adds to score based on word length. Establish score based on length of word in the dictionary to account for the Qu tile
+// function timer() {} // counts down from set time limit
+function gameOver() {} // upon timer end ends board click event, reveals final score, and displays replay button
 
-
+console.log(dictionary.length);
 
 // challenges
-// timer --- actually making it seems pretty simple based on using setTimeout or similar. Displaying it could be a bit tricky, or it could be a piece of cake. Only time(r) will tell.
+// timer // actually making it seems pretty simple based on using setTimeout or similar. Displaying it could be a bit tricky, or it could be a piece of cake. Only time(r) will tell.
 // establishing what's an allowable click when searching for a word. Maybe just find rowIdx and colIdx and only allow if within +/- 1 and hasn't been clicked. Maybe this isn't that challenging now that I type it out. Leaving this. Enjoy.
 // mostly linking to, but also checking the dictionary for word validity
