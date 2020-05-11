2b76444363f33d537a38a2f5a1dce2e40a901b7f
Shawn Gluchacki <Shawn@MacBook-Pro-2.local>
2020-05-11 11:33:48 -0600

    add functionality for display and submit words in progress

--- js/main.js
+++ js/main.js
@@ -2,7 +2,7 @@
 // Letter distribution via the distribution of tiles in Scrabble
 // Casing as listed for display convenience
 const letters = ['A','A','A','A','A','A','A','A','A','B','B','C','C','D','D','D','D','E','E','E','E','E','E','E','E','E','E','E','E','F','F','G','G','G','H','H','I','I','I','I','I','I','I','I','I','J','K','L','L','L','L','M','M','N','N','N','N','N','N','O','O','O','O','O','O','O','O','P','P','Qu','R','R','R','R','R','R','S','S','S','S','T','T','T','T','T','T','U','U','U','U','V','V','W','W','X','Y','Y','Z']
-const insults = ['Dats nat e werd.', 'Seriously?', 'Are you even trying?', 'Why do you even bother?', 'Ughh...', 'But... I mean... How?', 'Try again.', 'Remember Hooked on Phonics? Look into it.', 'Questionable, at best.', 'Practice makes perfect.', 'Consider resetting the board.', 'No.', 'No, no, and no.', 'Your skill never ceases to underwhelm me.', 'If I throw a stick, will you leave?', 'Your inferiority complex is fully justified.', 'Do you have delusions of adequacy?', 'I like the way you try.', "I like your approach. Now let's see your departure.", 'You fool!'];
+const insults = ['Dats nat e werd.', "That's not a word, silly!", 'Seriously?', 'Are you even trying?', 'Why do you even bother?', 'Ughh...', 'But... I mean... How?', 'Try again.', 'Remember Hooked on Phonics? Look into it.', 'Questionable, at best.', 'Practice makes perfect.', 'Consider resetting the board.', 'No.', 'No, no, and no.', 'Your skill never ceases to underwhelm me.', 'If I throw a stick, will you leave?', 'Your inferiority complex is fully justified.', 'Do you have delusions of adequacy?', 'I like the way you try.', "I like your approach. Now let's see your departure.", 'You fool!'];
 const accolades = ['Another one.', 'Nailed it.', 'Nice!', 'Great!', 'Next stop: Scripps Spelling Bee!', 'Genius!', 'Impressive.', 'Are you Will Shortz?', 'Nigel Richards? Is that you?', "You're on fire!", 'Keep up the good work!', 'Yippee ki-yay!', "You're a gift to those around you.", 'You are one smart cookie!', 'Inspiring!', "You're a candle in the dark.", 'Awesome!', 'Crushing it!', "Are you cheating? I feel like you're cheating.", "Is 'on fleek' still a thing? If it is, you're totally on fleek."];
 
 
@@ -22,9 +22,9 @@ let rowClickIdx;
 //Should this be an object with ricj indices?
 $boardArray = [...$('#board > .letters')]; // each div on the board 
 
-
+$currentWord = $('#current-word');
 $message = $('#message'); // for messages like 'That's not a word, silly!' and 'Time's up.'
-$submitButton = $('#message'); // button for checking word
+$submitButton = $('#submit'); // button for checking word
 $replayButton = $('#replay'); // button that pops up after game ends
 $score = $('#score'); // to display score
 $foundWords = $('#found-words'); // maybe. just displays list of already found words
@@ -34,6 +34,7 @@ $timer = $('timer'); // its a timer
 // event listeners
 // hope you have fast fingers, this is going to be the only input method
 $('#board').click(clickBoard);
+$submitButton.click(submitWord);
 $replayButton.click(init);  // replayClick; // calls init
 
 
@@ -58,6 +59,7 @@ function init() {
                             // rowIdx, colIdx, clickable, letter
                             // final answer. usedLetters and boardArray are associated arrays of objects
     wordInProgress = '';
+    $message.text('');
     render();
     renderBoard(); // randomizer in renderBoard don't call in render
 }
@@ -66,7 +68,9 @@ function init() {
 function render() {
     $score.text(`Score: ${score}`);
     // display found words as new li in found words ul
-    
+
+    // display wordInProgress
+    $currentWord.text(`${wordInProgress}`)
 
     //renderBoard();  Not here, you fool!
 } 
@@ -79,6 +83,7 @@ function renderBoard() {
         $($boardArray[i]).text(board[i]);  // WTF?!?!?!?!!?!?
         // console.log($boardArray[i]);
         letterObjects.push({
+            cellIdx: $($boardArray[i]).attr('id'),
             rowIdx: parseInt($($boardArray[i]).attr('id').slice(1,2)),
             colIdx: parseInt($($boardArray[i]).attr('id').slice(3,4)),
             letterValue: board[i],
@@ -105,6 +110,7 @@ function wordCheck(word) {
         $message.text(insults[Math.floor(Math.random() * insults.length)])   // same here, Math.random it
     }
     return isWordValid;     // necessary? 
+                            // Yes. See logic in submit word.
 } 
 
 // establishes what clicks are valid, and logs usedLetters and words in progress
@@ -115,12 +121,24 @@ function clickBoard(click) {
     rowClickIdx = parseInt(clickIdx.slice(1, 2));
     colClickIdx = parseInt(clickIdx.slice(3, 4));
 
+    // if target clickable is false, return
+    letterObjects.forEach(function (letterObject) {
+        if (letterObject.cellIdx === clickIdx) {
+            console.log('This should match what I clicked: ', letterObject);
+            if (letterObject.clickable === false) {
+                return;
+            }
+        }
+    });
 
     // good form to disable clicks before here. 
     // should be satisfactory to disable inside determineClick
     letterObjects.forEach(function(letterObject) {
         if (letterObject.rowIdx === rowClickIdx && letterObject.colIdx === colClickIdx) {
             usedLetters.push(letterObject);
+            //push letter to word in progress here
+            wordInProgress += letterObject.letterValue.toUpperCase();   //uppercase to account for 'Qu'
+            console.log('word in progress: ', wordInProgress);
         }
     });
 
@@ -129,6 +147,7 @@ function clickBoard(click) {
     // console.log('row index: ', rowClickIdx);
     // console.log('column index: ', colClickIdx);
     determineClickable();
+    render();   // to display wordInProgress
 } 
 
 function determineClickable() {
@@ -145,7 +164,8 @@ function determineClickable() {
         if (usedLetters.includes(letterObject)) {
             letterObject.clickable = false;
         }
-        console.log(letterObject);
+        // if clickable = false then remove target from listener
+        // console.log(letterObject);
     });
 }
 
@@ -165,7 +185,28 @@ function addScore(word) {
         score += 11
     }
     render();
-} 
+}
+
+
+// untested.
+// need to incorporate caching and display of wordInProgress
+function submitWord(click) {    
+    // on click if not in found words
+    // submit cached word to wordCheck();
+    if (foundWords.includes(wordInProgress)) {
+        // to avoid copies of a word and its effects on score
+        return;
+    } else {
+        wordCheck(wordInProgress);  // need to call function to return insult/accolade  
+        if (wordCheck(wordInProgress)) {
+            foundWords.push(wordInProgress);    // if word is valid, add to foundWords
+        }
+    }
+
+    // reset wordInProgress
+    wordInProgress = '';
+    
+}
 
 // counts down from set time limit
 function countDown() {} 
